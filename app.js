50442b76ea94941ab66282c33a25f665de445ad2
Zakhar Likay <zakhar.likay@mail.ru>
2022-04-21 16:01:12 +0700

    Next try

---
+++ app.js
@@ -0,0 +1,49 @@
+let operand1 = '0', operand2 = '0' //Operands
+let operatorLast = '+' //Operator
+const out = document.getElementById('out')
+
+const buttons = document.getElementsByClassName('btn')
+for(let i = 0; i < buttons.length; i++)
+    buttons[i].onclick = () => {
+    buttonClicked(buttons[i].id)
+  }
+
+function buttonClicked(id) {
+    const idN = parseInt(id)
+    if(!isNaN(idN)) {  //put numbers to display
+        if(operand2 == '0') {
+            operand2 = '' + idN
+            redrawDisplay()
+        } else {
+            operand2 += idN
+            redrawDisplay()
+        }
+    } else if(id == ','){
+        if (!operand2.includes('.')) {
+            operand2 += '.'
+            redrawDisplay()
+        }
+    } else if(['*','/','+','-'].includes(id)) {
+        operand2 = eval(operand1 + operatorLast + operand2) //Rewrite IT!!! Withput Eval()
+        redrawDisplay()
+        operatorLast = id
+        operand1 = operand2
+        operand2 = '0'
+        
+    } else if(id == '=') {
+        operand2 = eval(operand1 + operatorLast + operand2) //Rewrite IT!!! Withput Eval()
+        redrawDisplay()
+        operand1 = '0'
+        operand2 = '0'
+        operatorLast = '+'
+    }
+    
+
+
+    
+    console.log(typeof(id))
+}
+
+function redrawDisplay() {
+    out.textContent = operand2
+}
